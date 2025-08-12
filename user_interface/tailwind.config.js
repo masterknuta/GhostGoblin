7e55215745640c279ab17be6ca101e8a1fc49669
Master KnutA <aquarius.ai.cloud@gmail.com>
2025-08-12 20:14:21 +0200

    Add Tailwind setup, index.css, and config files

---
+++ user_interface/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // adjust this if your source files are in a different folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
