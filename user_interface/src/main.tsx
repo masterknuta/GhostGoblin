067a0b951e313e8b563618adfeda37dc610d8dc8
Master KnutA <aquarius.ai.cloud@gmail.com>
2025-08-13 22:33:56 +0200

    replaced index.html

--- user_interface/src/main.tsx
+++ user_interface/src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// Find the root element in the HTML.
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  // Create a React root and render the App component.
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+} else {
+  // Log an error if the root element isn't found.
+  console.error("Root element with ID 'root' not found in the document.");
+}
