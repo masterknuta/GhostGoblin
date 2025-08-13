0e357b2c9789805f3fb220d9840fa76d266a5f49
Master KnutA <aquarius.ai.cloud@gmail.com>
2025-08-13 22:11:15 +0200

    changed vite.config

--- user_interface/vite.config.ts
+++ user_interface/vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: '..', // This tells Vite to look for the project root (where index.html is) one directory up.
   plugins: [react()],
 })
