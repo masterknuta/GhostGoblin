775c6d5f2abaa8cb7a9e3abd894ee01b74cf948c
Master KnutA <aquarius.ai.cloud@gmail.com>
2025-08-14 15:12:11 +0200

    trying to fix vite.config

--- user_interface/vite.config.ts
+++ user_interface/vite.config.ts
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: '.',          // user_interface folder is root
-  build: {
-    outDir: 'dist',   // output goes to user_interface/dist
-    emptyOutDir: true,
-    rollupOptions: {
-      input: 'index.html', // entry point relative to root
-    },
-  },
 })
